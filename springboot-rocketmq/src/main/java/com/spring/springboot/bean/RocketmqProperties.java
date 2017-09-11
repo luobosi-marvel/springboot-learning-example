5dad2a3f06ae3009ef9a76780b4d4ce902aa35a5
luobosi <luobosi@2dfire.com>
2017-09-11 21:16:20 +0800

    rocketmq

---
+++ springboot-rocketmq/src/main/java/com/spring/springboot/bean/RocketmqProperties.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.spring.springboot.bean;
+
+/**
+ * RocketmqProperties
+ *
+ * @author 萝卜丝
+ * @since 2017-09-11
+ */
+public class RocketmqProperties {
+
+}
