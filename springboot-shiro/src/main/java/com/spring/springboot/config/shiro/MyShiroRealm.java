429dda29e67fbd18be5973db2fa1d02f5726dad5
luobosi <luobosi@2dfire.com>
2017-09-12 21:12:49 +0800

    SpringBoot 整合 Shiro

---
+++ springboot-shiro/src/main/java/com/spring/springboot/config/shiro/MyShiroRealm.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.spring.springboot.config.shiro;
+
+import com.spring.springboot.bean.SysPermission;
+import com.spring.springboot.bean.SysRole;
+import com.spring.springboot.bean.UserInfo;
+import com.spring.springboot.service.UserInfoService;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+import javax.annotation.Resource;
+
+/**
+ * MyShiroRealm
+ *
+ * @author 萝卜丝
+ * @since 2017-09-12
+ */
+public class MyShiroRealm extends AuthorizingRealm {
+
+    @Resource
+    private UserInfoService userInfoService;
+
+    /**
+     * 认证信息.(身份验证)
+     * :
+     * Authentication 是用来验证用户身份
+     * 主要是用来进行身份认证的，也就是说验证用户输入的账号和密码是否正确。
+     * @param token 身份认证信息
+     * @return 认证之后的信息
+     * @throws AuthenticationException 验证不通过则抛出异常
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        System.out.println("MyShiroRealm.doGetAuthenticationInfo()");
+        //获取用户的输入的账号.
+        String name = (String)token.getPrincipal();
+        System.out.println(name);
+        System.out.println(token.getCredentials());
+
+        //通过username从数据库中查找 User对象，如果找到，没找到.
+        //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
+        UserInfo userInfo = userInfoService.findByName(name);
+        System.out.println("----->> userInfo = "+userInfo);
+        if(userInfo == null){
+            return null;
+        }
+
+        /*
+         * 获取权限信息:这里没有进行实现，
+         * 请自行根据UserInfo,Role,Permission进行实现；
+         * 获取之后可以在前端for循环显示所有链接;
+         */
+        //userInfo.setPermissions(userService.findPermissions(user));
+
+
+        //账号判断;
+
+        //加密方式;
+        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
+
+        //明文: 若存在，将此用户存放到登录认证info中，无需自己做密码对比，Shiro会为我们进行密码对比校验
+//      SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+//           userInfo, //用户名
+//           userInfo.getPassword(), //密码
+//             getName()  //realm name
+//      );
+
+        return new SimpleAuthenticationInfo(
+                userInfo, //用户名
+                userInfo.getPassword(), //密码
+                ByteSource.Util.bytes(userInfo.getCredentialsSalt()),//salt=username+salt
+                getName()  //realm name
+        );
+    }
+
+
+
+    /**
+     * 此方法调用  hasRole,hasPermission的时候才会进行回调.
+     * 是权限控制，当访问到页面的时候，使用了相应的注解或者shiro标签才会执行此方法否则不会执行，
+     * 所以如果只是简单的身份认证没有权限的控制的话，那么这个方法可以不进行实现，直接返回null即可。
+     *
+     *
+     * 权限信息.(授权):
+     * 1、如果用户正常退出，缓存自动清空；
+     * 2、如果用户非正常退出，缓存自动清空；
+     * 3、如果我们修改了用户的权限，而用户不退出系统，修改的权限无法立即生效。
+     * （需要手动编程进行实现；放在service进行调用）
+     * 在权限修改后调用realm中的方法，realm已经由spring管理，所以从spring中获取realm实例，
+     * 调用clearCached方法；
+     * :Authorization 是授权访问控制，用于对用户进行的操作授权，证明该用户是否允许进行当前操作，如访问某个链接，某个资源文件等。
+         * @param principals 身份及凭据
+     * @return 权限验证
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+       /*
+        * 当没有使用缓存的时候，不断刷新页面的话，这个代码会不断执行，
+        * 当其实没有必要每次都重新设置权限信息，所以我们需要放到缓存中进行管理；
+        * 当放到缓存中时，这样的话，doGetAuthorizationInfo就只会执行一次了，
+        * 缓存过期之后会再次执行。
+        */
+        System.out.println("权限配置 --> MyShiroRealm.doGetAuthorizationInfo()");
+
+        // SimpleAuthorizationInfo: 进行角色的添加和权限的添加。
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        UserInfo userInfo  = (UserInfo)principals.getPrimaryPrincipal();
+
+        //实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
+//     UserInfo userInfo = userInfoService.findByUsername(username)
+
+
+        //权限单个添加;
+        // 或者按下面这样添加
+        //添加一个角色,不是配置意义上的添加,而是证明该用户拥有admin角色
+//     authorizationInfo.addRole("admin");
+        //添加权限
+//     authorizationInfo.addStringPermission("userInfo:query");
+
+
+
+        ///在认证成功之后返回.
+        //设置角色信息.
+        //支持 Set集合,
+        //用户的角色对应的所有权限，如果只使用角色定义访问权限，下面的四行可以不要
+//        List<Role> roleList=user.getRoleList();
+//        for (Role role : roleList) {
+//            info.addStringPermissions(role.getPermissionsName());
+//        }
+        for(SysRole role:userInfo.getRoleList()){
+            authorizationInfo.addRole(role.getRole());
+            for(SysPermission p:role.getPermissions()){
+                authorizationInfo.addStringPermission(p.getPermission());
+            }
+        }
+
+        //设置权限信息.
+//     authorizationInfo.setStringPermissions(getStringPermissions(userInfo.getRoleList()));
+
+        return authorizationInfo;
+    }
+
+
+/*
+    *
+     * 将权限对象中的权限code取出.
+     * @param permissions 权限
+     * @return 权限集合
+*/
+//  public Set<String> getStringPermissions(Set<SysPermission> permissions){
+//     Set<String> stringPermissions = new HashSet<String>();
+//     if(permissions != null){
+//         for(SysPermission p : permissions) {
+//            stringPermissions.add(p.getPermission());
+//          }
+//     }
+//       return stringPermissions;
+//  }
+
+}
---
+++ springboot-shiro/src/main/java/com/spring/springboot/controller/UserInfoController.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.spring.springboot.controller;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * UserInfoController
+ *
+ * @author 萝卜丝
+ * @since 2017-09-12
+ */
+@Controller
+@RequestMapping("/userInfo")
+public class UserInfoController {
+
+    /**
+     * 用户查询.
+     * @return 返回用户列表的逻辑视图名
+     */
+    @GetMapping("/userList")
+    public String userInfo(){
+        return "userInfo";
+    }
+
+    /**
+     * 用户添加;
+     * @return 返回用户添加的逻辑视图名
+     */
+    @GetMapping("/userAdd")
+    @RequiresPermissions("userInfo:add")//权限管理;
+    public String userInfoAdd(){
+        return "userInfoAdd";
+    }
+
+    /**
+     * 用户删除;
+     */
+    @RequestMapping("/userDel")
+    @RequiresPermissions("userInfo:del")//权限管理;
+    public String userDel(){
+        return "userInfoDel";
+    }
+}
