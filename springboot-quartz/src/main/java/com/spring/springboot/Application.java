3f7294fd665105fd6b3456fe3843e584e6c814cd
luobosi <luobosi@2dfire.com>
2017-09-13 22:09:45 +0800

    SpringBoot 整合 Quartz

---
+++ springboot-quartz/src/main/java/com/spring/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * TODO: @EnableScheduling 启动定时任务
+ */
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
