186d29822c1da60ada9082f529d92d0b2d398d58
luobosi <luobosi@2dfire.com>
2017-10-11 11:44:57 +0800

    忽略 marvel 文件

---
+++ springboot-shiro/src/main/java/com/spring/springboot/config/Config.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.spring.springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Config
+ *
+ * @author 萝卜丝
+ * @since 2017-09-12
+ */
+@Configuration
+public class Config extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+}
