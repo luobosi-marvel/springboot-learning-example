9e3c305e15d340948745b54b8041728e4aacf587
luobosi <luobosi@2dfire.com>
2017-10-21 10:23:58 +0800

    SpringBoot整合携程的 Apollo

---
+++ springboot-apollo/src/main/java/com/springboot/apollo/SpringbootApolloApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApolloApplication.class, args);
+	}
+}
