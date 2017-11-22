bb7656574192237812d11bc113e98e4264b674e6
luobosi <luobosi@2dfire.com>
2017-11-22 09:45:59 +0800

    添加自定义分页

---
+++ springboot-custom-paging/src/main/java/com/spring/springboot/page/BaseController.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2009-2016 Hangzhou 2Dfire Technology Co., Ltd. All rights reserved
+ */
+package com.spring.springboot.page;
+
+/**
+ * BaseController
+ *
+ * @author luobosi@2dfire.com
+ * @since 2017-11-22
+ */
+public class BaseController {
+
+}
