0d9102ee47e78936b25fe9b3286e1e39c2a66f5c
luobosi <luobosi@2dfire.com>
2017-11-24 14:43:33 +0800

    微调

--- springboot-dubbo-server/src/main/java/org/spring/springboot/dubbo/impl/CityDubboServiceImpl.java
+++ springboot-dubbo-server/src/main/java/org/spring/springboot/dubbo/impl/CityDubboServiceImpl.java
@@ -11,7 +11,7 @@ import org.spring.springboot.dubbo.CityDubboService;
 // 注册为 Dubbo 服务
 @Service(version = "1.0.0")
 public class CityDubboServiceImpl implements CityDubboService {
-
+    @Override
     public City findCityByName(String cityName) {
         return new City(1L,2L,"株洲","是我的故乡");
     }
